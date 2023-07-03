1edaa1a611de3e7d984796d72d13554b40478f60
AlcorBBX <lev.matisov55@gmail.com>
2023-07-03 21:50:39 +0500

    feat(NotFoundPage): add i18n

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,5 +1,7 @@
 import { memo } from 'react';
 
+import { useTranslate } from '@ayub-begimkulov/i18n';
+
 import { classNames } from 'shared/lib/classNames/classNames';
 
 import cls from './NotFoundPage.module.scss';
@@ -9,9 +11,11 @@ interface NotFoundPageProps {
 }
 
 export const NotFoundPage = memo(({ className }: NotFoundPageProps) => {
+  const t = useTranslate();
+
   return (
     <div className={classNames(cls.notfoundpage, {}, [className])}>
-      Страница не найдена
+      {t('Not found page')}
     </div>
   );
 });

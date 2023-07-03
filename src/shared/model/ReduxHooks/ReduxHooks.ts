9c209f9c4b7bc463faa57e39f4b9e72480d5bed1
AlcorBBX <lev.matisov55@gmail.com>
2023-07-03 20:44:04 +0500

    feat(store): add redux hooks

--- src/shared/model/ReduxHooks/ReduxHooks.ts
+++ src/shared/model/ReduxHooks/ReduxHooks.ts
@@ -4,5 +4,8 @@ import {
   useSelector,
 } from 'react-redux';
 
-// export const useAppDispatch = useDispatch<AppDispatch>;
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+// eslint-disable-next-line
+export const useAppDispatch = useDispatch<AppDispatch>;
+
+// eslint-disable-next-line
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

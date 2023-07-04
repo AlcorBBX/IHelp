3c5462a8f42dd39dd32cef25d6d24c94cd5113f6
AlcorBBX <lev.matisov55@gmail.com>
2023-07-04 18:08:05 +0500

    stories(Card.stories.tsx): add themes template

--- src/shared/ui/Card/__stories__/Card.stories.tsx
+++ src/shared/ui/Card/__stories__/Card.stories.tsx
@@ -14,7 +14,7 @@ export default {
 
 const DefaultTemplate: StoryFn<CardProps> = (args) => (
   <div>
-    <Card size="default" theme="backgroundInverted" {...args}>
+    <Card size="default" theme="primary" {...args}>
       <div>Card's background inverted</div>
     </Card>
 
@@ -28,6 +28,7 @@ export const Default = DefaultTemplate.bind({});
 const FullTemplate: StoryFn<CardProps> = (args) => (
   <div>
     <Card
+      theme="primary"
       title="Title"
       // cover={<p>d</p>}
       extra={<Button theme="primary">X</Button>}
@@ -44,7 +45,6 @@ const FullTemplate: StoryFn<CardProps> = (args) => (
         ],
       ]}
       size="default"
-      theme="background"
       {...args}
     >
       <div>Card's background</div>
@@ -52,3 +52,37 @@ const FullTemplate: StoryFn<CardProps> = (args) => (
   </div>
 );
 export const Full = FullTemplate.bind({});
+
+const ThemesTemplate: StoryFn<CardProps> = (args) => (
+  <div>
+    <Card size="default" theme="primary" {...args}>
+      <div>primary</div>
+    </Card>
+
+    <div style={{ background: 'grey', padding: '10px 0px 10px 5px' }}>
+      <Card size="default" theme="background" {...args}>
+        <div>background</div>
+      </Card>
+    </div>
+
+    <span style={{ margin: '16px' }} />
+    <Card size="default" theme="clear" {...args}>
+      <div>clear</div>
+    </Card>
+
+    <span style={{ margin: '16px' }} />
+    <Card size="default" theme="clearInverted" {...args}>
+      <div>clear invverted</div>
+    </Card>
+
+    <span style={{ margin: '16px' }} />
+    <Card size="default" theme="outline" {...args}>
+      <div>outline</div>
+    </Card>
+    <span style={{ margin: '16px' }} />
+    <Card size="default" theme="outlinedDanger" {...args}>
+      <div>outlined danger</div>
+    </Card>
+  </div>
+);
+export const Themes = ThemesTemplate.bind({});

159ba22f703923952289319c7a4f892806bdd32e
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2023-07-06 01:07:11 +0500

    stories(Skeleton): add skeleton template

---
+++ src/shared/ui/Skeleton/__stories__/Skeleton.stories.tsx
@@ -0,0 +1,96 @@
+import type { Meta, StoryFn } from '@storybook/react';
+
+import 'shared/styles/index.scss';
+
+import { Skeleton } from '../ui/Skeleton';
+import type { SkeletonProps } from '../ui/Skeleton.type';
+
+export default {
+  title: 'UiKit/Skeleton',
+  component: Skeleton,
+} as Meta;
+
+const DefaultTemplate: StoryFn<SkeletonProps> = (args) => (
+  <Skeleton {...args} />
+);
+export const Default = DefaultTemplate.bind({});
+
+const VariantTemplate: StoryFn<SkeletonProps> = (args) => (
+  <div>
+    <p>Variant text</p>
+    <Skeleton {...args} variant="text" />
+    <span style={{ margin: '16px' }} />
+    <p>Variant rounded</p>
+    <Skeleton {...args} variant="rounded" />
+    <span style={{ margin: '16px' }} />
+    <p>Variant rectangular</p>
+    <Skeleton {...args} variant="rectangular" />
+    <span style={{ margin: '16px' }} />
+    <p>Variant circular</p>
+    <Skeleton {...args} variant="circular" />
+  </div>
+);
+export const Variant = VariantTemplate.bind({});
+
+const SizeTemplate: StoryFn<SkeletonProps> = (args) => (
+  <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+    <div>
+      <p style={{ marginBottom: '16px', fontSize: '26px' }}>Variant text</p>
+      <p>Size small</p>
+      <Skeleton {...args} size="small" />
+      <span style={{ margin: '16px' }} />
+      <p>Size default</p>
+      <Skeleton {...args} size="default" />
+      <span style={{ margin: '16px' }} />
+      <p>Size large</p>
+      <Skeleton {...args} size="large" />
+    </div>
+    <div>
+      <p style={{ marginBottom: '16px', fontSize: '26px' }}>Variant rounded</p>
+      <p>Size small</p>
+      <Skeleton {...args} variant="rounded" size="small" />
+      <span style={{ margin: '16px' }} />
+      <p>Size default</p>
+      <Skeleton {...args} variant="rounded" size="default" />
+      <span style={{ margin: '16px' }} />
+      <p>Size large</p>
+      <Skeleton {...args} variant="rounded" size="large" />
+    </div>
+    <div>
+      <p style={{ marginBottom: '16px', fontSize: '26px' }}>
+        Variant rectangular
+      </p>
+      <p>Size small</p>
+      <Skeleton {...args} variant="rectangular" size="small" />
+      <span style={{ margin: '16px' }} />
+      <p>Size default</p>
+      <Skeleton {...args} variant="rectangular" size="default" />
+      <span style={{ margin: '16px' }} />
+      <p>Size large</p>
+      <Skeleton {...args} variant="rectangular" size="large" />
+    </div>
+    <div>
+      <p style={{ marginBottom: '16px', fontSize: '26px' }}>Variant circular</p>
+      <p>Size small</p>
+      <Skeleton {...args} variant="circular" size="small" />
+      <span style={{ margin: '16px' }} />
+      <p>Size default</p>
+      <Skeleton {...args} variant="circular" size="default" />
+      <span style={{ margin: '16px' }} />
+      <p>Size large</p>
+      <Skeleton {...args} variant="circular" size="large" />
+    </div>
+  </div>
+);
+export const Size = SizeTemplate.bind({});
+
+const AnimationTemplate: StoryFn<SkeletonProps> = (args) => (
+  <div>
+    <p>Animation wave</p>
+    <Skeleton {...args} animation="wave" />
+    <span style={{ margin: '16px' }} />
+    <p>Animation pulse</p>
+    <Skeleton {...args} animation="pulse" />
+  </div>
+);
+export const Animation = AnimationTemplate.bind({});

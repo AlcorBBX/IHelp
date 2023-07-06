2c012230bc51ccf2064f99cbffb67eda5bf79261
AlcorBBX <lev.matisov55@gmail.com>
2023-07-06 13:03:21 +0500

    refactor(StoreProvider): add index import

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -6,12 +6,11 @@ import {
   ErrorBoundary,
   I18Provider,
   RouterProvider,
+  StoreProvider,
   ThemeProvider,
 } from 'app/providers';
 
 import 'shared/styles/index.scss';
-
-import { StoreProvider } from './providers/StoreProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -3,3 +3,4 @@ export { ErrorBoundary, BugButton } from './ErrorBoundary';
 export { ThemeProvider } from './ThemeProvider';
 export { I18Provider } from './I18Provider';
 export { FirebaseProvider } from './FirebaseProvider';
+export { StoreProvider } from './StoreProvider';

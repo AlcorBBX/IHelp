eeea6d6ba6a706443a0bb8e91c8a1e0ab4a25b85
AlcorBBX <lev.matisov55@gmail.com>
2023-07-08 00:27:41 +0500

    feat(ThemeContext.ts): change type

--- src/shared/lib/theme/ThemeContext.ts
+++ src/shared/lib/theme/ThemeContext.ts
@@ -1,13 +1,5 @@
 import { createContext } from 'react';
 
-export enum Theme {
-  STEALTH = 'stealth',
-  CHAOS_THEORY = 'chaos_theory',
-  DARK_NOTE = 'dark_note',
-  DARK = 'dark',
-  LIGHT = 'light',
-}
-
 export const themes = {
   stealth: 'stealth',
   chaos_theory: 'chaos theory',
@@ -29,8 +21,8 @@ export const themesEntries = Object.entries(themes);
 // export type ThemesType = ReturnType<typeof themes>
 
 export interface ThemeContextProps {
-  theme?: Theme;
-  setTheme?: (theme: Theme) => void;
+  theme?: typeof themes;
+  setTheme?: (theme: typeof themes) => void;
 }
 
 export const ThemeContext = createContext<ThemeContextProps>({});

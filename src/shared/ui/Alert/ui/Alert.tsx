92bec740f5d488bd81179995e7f116b817283a4d
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2023-07-09 17:23:04 +0500

    feat(Alert.tsx): added portal into alert

--- src/shared/ui/Alert/ui/Alert.tsx
+++ src/shared/ui/Alert/ui/Alert.tsx
@@ -2,6 +2,7 @@ import { forwardRef } from 'react';
 
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Icon } from 'shared/ui/Icon';
+import { Portal } from 'shared/ui/Portal/Portal';
 
 import cls from './Alert.module.scss';
 import { AlertProps } from './Alert.type';
@@ -15,12 +16,15 @@ export const Alert = forwardRef<HTMLDivElement, AlertProps>((props, ref) => {
 
   // TODO make a choice of icon
   return (
-    <div ref={ref} className={classNames(cls.alert, mods, [className])}>
-      <Icon type="profile" />
-      <div>
-        {title ? <p className={cls.title}>{title}</p> : null}
-        <p className={cls.message}>{message}</p>
+    <Portal>
+      <div ref={ref} className={classNames(cls.alert, mods, [className])}>
+        <Icon type="profile" />
+        <div>
+          {title ? <p className={cls.title}>{title}</p> : null}
+          <p className={cls.message}>{message}</p>
+        </div>
+        <Icon type="exit" />
       </div>
-    </div>
+    </Portal>
   );
 });
